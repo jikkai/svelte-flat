bffe6506f6ee13d09a3ab2f38ee4da0531c55bc7
十界 <sonne@asaki.me>
2016-12-28 00:33:57 +0800

    add normalize style

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 import Routers from './router'
 
-import 'han-css/dist/han.min.css'
+import '../../packages/theme-default/dist/common.css'
 import './themes/index'
 
 ReactDOM.render(
