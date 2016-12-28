cfdaeefbee4a9f92a4925479ea45a905297fb50e
Jikkai Xiao <sonne@asaki.me>
2016-12-28 16:24:08 +0800

    update docs

--- examples/src/router/Icon.js
+++ examples/src/router/Icon.js
@@ -2,117 +2,74 @@ import React from 'react'
 import '../../../packages/theme-default/dist/icon.css'
 
 class IconRouter extends React.Component {
+  constructor (params) {
+    super(params)
+    this.state = {
+      icon: {
+        glyphs: [
+          'fui-info-circle', 'fui-alert-circle', 'fui-question-circle', 'fui-check-circle',
+          'fui-cross-circle', 'fui-plus-circle', 'fui-window', 'fui-windows', 'fui-upload',
+          'fui-arrow-right', 'fui-arrow-left', 'fui-loop', 'fui-cmd', 'fui-mic', 'fui-export',
+          'fui-heart', 'fui-location', 'fui-plus', 'fui-check', 'fui-cross', 'fui-list',
+          'fui-new', 'fui-video', 'fui-photo', 'fui-time', 'fui-eye', 'fui-chat', 'fui-home',
+          'fui-search', 'fui-user', 'fui-mail', 'fui-lock', 'fui-power', 'fui-star',
+          'fui-calendar', 'fui-gear', 'fui-bookmark', 'fui-exit', 'fui-trash', 'fui-folder',
+          'fui-bubble', 'fui-calendar-solid', 'fui-star-2', 'fui-credit-card', 'fui-clip',
+          'fui-link', 'fui-pause', 'fui-play', 'fui-volume', 'fui-mute', 'fui-resize', 'fui-tag',
+          'fui-document', 'fui-image', 'fui-triangle-up', 'fui-triangle-up-small',
+          'fui-triangle-right-large', 'fui-triangle-left-large', 'fui-triangle-down',
+          'fui-triangle-down-small', 'fui-radio-unchecked', 'fui-radio-checked',
+          'fui-checkbox-unchecked', 'fui-checkbox-checked', 'fui-list-thumbnailed',
+          'fui-list-small-thumbnails', 'fui-list-numbered', 'fui-list-large-thumbnails',
+          'fui-list-columned', 'fui-list-bulleted'
+        ],
+        social: [
+          'fui-facebook', 'fui-youtube', 'fui-vimeo', 'fui-twitter',
+          'fui-stumbleupon', 'fui-spotify', 'fui-skype', 'fui-pinterest',
+          'fui-path', 'fui-linkedin', 'fui-google-plus', 'fui-dribbble',
+          'fui-behance', 'fui-yelp', 'fui-wordpress', 'fui-windows-8', 'fui-vine',
+          'fui-tumblr', 'fui-paypal', 'fui-lastfm', 'fui-instagram', 'fui-html5',
+          'fui-github', 'fui-foursquare', 'fui-dropbox', 'fui-android', 'fui-apple'
+        ]
+      }
+    }
+  }
   render () {
+    const { icon } = this.state
+
     return (
       <div className="examples-router examples-router-icon">
         <h2>Icon 图标</h2>
 
         <section className="examples">
-          <h3>Glyphs (70)</h3>
+          <h3>Glyphs ({icon.glyphs.length})</h3>
           <div>
-            <span className="fui-info-circle"></span>
-            <span className="fui-alert-circle"></span>
-            <span className="fui-question-circle"></span>
-            <span className="fui-check-circle"></span>
-            <span className="fui-cross-circle"></span>
-            <span className="fui-plus-circle"></span>
-            <span className="fui-window"></span>
-            <span className="fui-windows"></span>
-            <span className="fui-upload"></span>
-            <span className="fui-arrow-right"></span>
-            <span className="fui-arrow-left"></span>
-            <span className="fui-loop"></span>
-            <span className="fui-cmd"></span>
-            <span className="fui-mic"></span>
-            <span className="fui-export"></span>
-            <span className="fui-heart"></span>
-            <span className="fui-location"></span>
-            <span className="fui-plus"></span>
-            <span className="fui-check"></span>
-            <span className="fui-cross"></span>
-            <span className="fui-list"></span>
-            <span className="fui-new"></span>
-            <span className="fui-video"></span>
-            <span className="fui-photo"></span>
-            <span className="fui-time"></span>
-            <span className="fui-eye"></span>
-            <span className="fui-chat"></span>
-            <span className="fui-home"></span>
-            <span className="fui-search"></span>
-            <span className="fui-user"></span>
-            <span className="fui-mail"></span>
-            <span className="fui-lock"></span>
-            <span className="fui-power"></span>
-            <span className="fui-star"></span>
-            <span className="fui-calendar"></span>
-            <span className="fui-gear"></span>
-            <span className="fui-bookmark"></span>
-            <span className="fui-exit"></span>
-            <span className="fui-trash"></span>
-            <span className="fui-folder"></span>
-            <span className="fui-bubble"></span>
-            <span className="fui-calendar-solid"></span>
-            <span className="fui-star-2"></span>
-            <span className="fui-credit-card"></span>
-            <span className="fui-clip"></span>
-            <span className="fui-link"></span>
-            <span className="fui-pause"></span>
-            <span className="fui-play"></span>
-            <span className="fui-volume"></span>
-            <span className="fui-mute"></span>
-            <span className="fui-resize"></span>
-            <span className="fui-tag"></span>
-            <span className="fui-document"></span>
-            <span className="fui-image"></span>
-            <span className="fui-triangle-up"></span>
-            <span className="fui-triangle-up-small"></span>
-            <span className="fui-triangle-right-large"></span>
-            <span className="fui-triangle-left-large"></span>
-            <span className="fui-triangle-down"></span>
-            <span className="fui-triangle-down-small"></span>
-            <span className="fui-radio-unchecked"></span>
-            <span className="fui-radio-checked"></span>
-            <span className="fui-checkbox-unchecked"></span>
-            <span className="fui-checkbox-checked"></span>
-            <span className="fui-list-thumbnailed"></span>
-            <span className="fui-list-small-thumbnails"></span>
-            <span className="fui-list-numbered"></span>
-            <span className="fui-list-large-thumbnails"></span>
-            <span className="fui-list-columned"></span>
-            <span className="fui-list-bulleted"></span>
+            {
+              icon.glyphs.map((item, i) => {
+                return (
+                  <div key={i}>
+                    <i className={item}></i>
+                    <span>{item}</span>
+                  </div>
+                )
+              })
+            }
           </div>
         </section>
 
         <section className="examples">
-          <h3>Social (27)</h3>
+          <h3>Social ({icon.social.length})</h3>
           <div>
-            <span className="fui-facebook"></span>
-            <span className="fui-youtube"></span>
-            <span className="fui-vimeo"></span>
-            <span className="fui-twitter"></span>
-            <span className="fui-stumbleupon"></span>
-            <span className="fui-spotify"></span>
-            <span className="fui-skype"></span>
-            <span className="fui-pinterest"></span>
-            <span className="fui-path"></span>
-            <span className="fui-linkedin"></span>
-            <span className="fui-google-plus"></span>
-            <span className="fui-dribbble"></span>
-            <span className="fui-behance"></span>
-            <span className="fui-yelp"></span>
-            <span className="fui-wordpress"></span>
-            <span className="fui-windows-8"></span>
-            <span className="fui-vine"></span>
-            <span className="fui-tumblr"></span>
-            <span className="fui-paypal"></span>
-            <span className="fui-lastfm"></span>
-            <span className="fui-instagram"></span>
-            <span className="fui-html5"></span>
-            <span className="fui-github"></span>
-            <span className="fui-foursquare"></span>
-            <span className="fui-dropbox"></span>
-            <span className="fui-android"></span>
-            <span className="fui-apple"></span>
+            {
+              icon.social.map((item, i) => {
+                return (
+                  <div key={i}>
+                    <i className={item}></i>
+                    <span>{item}</span>
+                  </div>
+                )
+              })
+            }
           </div>
         </section>
       </div>
