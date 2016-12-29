651c5ad15a5e381c1be3c36a08ecf641a98a3127
Jikkai Xiao <sonne@asaki.me>
2016-12-29 14:32:33 +0800

    build components with rollup

---
+++ packages/input/rollup.config.js
@@ -0,0 +1,13 @@
+export default {
+  entry: './src/index.html',
+  plugins: [
+    require('rollup-plugin-svelte')(),
+    require('rollup-plugin-uglify')(),
+    require('rollup-plugin-babel')({
+      exclude: 'node_modules/**'
+    })
+  ],
+  format: 'umd',
+  moduleName: "Input",
+  dest: './dist/index.js'
+}
