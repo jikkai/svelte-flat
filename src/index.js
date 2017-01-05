b3deea08c55cbbf718dcccf64179d311b4c8dc36
Jikkai Xiao <sonne@asaki.me>
2017-01-05 17:13:59 +0800

    Radio: add radio component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import Button from '../packages/button/src/index.html'
-import Input from '../packages/input/src/index.html'
-import Checkbox from '../packages/checkbox/src/index.html'
-import Slider from '../packages/slider/src/index.html'
+import Button from '../packages/button/src/index.js'
+import Input from '../packages/input/src/index.js'
+import Checkbox from '../packages/checkbox/src/index.js'
+import Radio from '../packages/radio/src/index.js'
+import Slider from '../packages/slider/src/index.js'
 
-import Video from '../packages/video/src/index.html'
+import Video from '../packages/video/src/index.js'
 
-import Message from '../packages/message/src/index'
-import Loading from '../packages/loading/src/index'
+import Message from '../packages/message/src/index.js'
+import Loading from '../packages/loading/src/index.js'
 
 export default { 
   Button,
   Input,
   Checkbox,
+  Radio,
   Slider,
 
   Video,
