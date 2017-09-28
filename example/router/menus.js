5494a6ac610a144426506133427b98e4984c6d81
Jikkai Xiao <sonne@asaki.me>
2017-09-28 21:15:08 +0800

    [Button]update new props

---
+++ example/router/menus.js
@@ -0,0 +1,10 @@
+export default [
+  { title: '首页', to: '/guide/intro' },
+  {
+    title: '基础组件',
+    submenus: [
+      { title: 'Color 色彩', to: '/components/color' },
+      { title: 'Button 按钮', to: '/components/button' }
+    ]
+  }
+]
--- example/router/routes.js
+++ example/router/routes.js
@@ -1,10 +1,12 @@
 import Home from './Root/Home.html'
 import Intro from './Guide/Intro.html'
+import Color from './Components/Color.html'
 import Button from './Components/Button.html'
 
 const routes = {
   '/': Home,
   '/guide/intro': Intro,
+  '/components/color': Color,
   '/components/button': Button
 }
 
