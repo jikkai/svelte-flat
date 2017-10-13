d2dfece6cb26e799293b4b27daae7ccf0a8b3fa8
Jikkai Xiao <sonne@asaki.me>
2017-10-13 18:29:23 +0800

    fix entry

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import Button from '~packages/button'
+import _Icon from '~packages/icon'
+import _Button from '~packages/button'
+import _Switch from '~packages/switch'
+import _Message from '~packages/message'
+import _Confirm from '~packages/confirm'
+import _Loading from '~packages/loading'
+import _LoadingBar from '~packages/loading-bar'
 
 export default {
-  Button
+  Icon: _Icon,
+  Button: _Button,
+  Switch: _Switch,
+  Message: _Message,
+  Confirm: _Confirm,
+  Loading: _Loading,
+  LoadingBar: _LoadingBar
 }
+
+export const Icon = _Icon
+export const Button = _Button
+export const Switch = _Switch
+export const Message = _Message
+export const Confirm = _Confirm
+export const Loading = _Loading
+export const LoadingBar = _LoadingBar
